c1dc9110c19c1af5cc55e4d53b22238809ad40de
bcqsd <20204248@cqu.edu.cn>
2022-03-29 23:48:23 +0800

    add post method

--- src/test/1.js
+++ src/test/1.js
@@ -15,7 +15,7 @@ function handler(req,res){
         const dataPath=path.join(__dirname,`../data/${name}.json`)
         fs.readFile(dataPath,'utf8',(err,data)=>{
             if(err){
-                res.statusCode=500
+               errorHandler(500,err)
             }
             res.statusCode=200
             res.setHeader('Content-Type','application/json')
@@ -24,7 +24,44 @@ function handler(req,res){
         })
         return 
     }
-    res.end('Hello Node\n')
+    else if(method=='POST'&&pathname=='/api/v1/user'){
+        const buffer=[]
+        req.on('data',chunk=>{
+            buffer.push(chunk)
+        })
+        req.on('end',()=>{
+            const insert=JSON.parse(Buffer.concat(buffer).toString())
+            if(insert.name==null){
+               errorHandler(422,{message:'name required',statusCode:422,statusMessage:'bad request'})
+               return
+            }
+            const dataPath=path.join(__dirname,`../data/${insert.name}.json`)
+            fs.writeFile(dataPath,JSON.stringify(insert),'utf8',(err)=>{
+                if(err){
+                    console.error('写入失败')
+                     errorHandler(500,err)
+                }
+                res.status=200
+                res.statusCode=200
+                res.setHeader('Content-Type','application/json')
+                res.end(JSON.stringify({
+                    success:true,
+                    message:'写入成功'
+                }))
+            })
+        })
+        return 
+    }
+    function errorHandler(status,err){
+        console.error(`[Error]${req.method}${req.url},err`)
+        res.statusCode=status
+        res.statusMessage=err?.statusMessage
+        res.setHeader('Content-Type','application/json')
+        res.end(JSON.stringify({
+            success:false,
+            message:err?.message
+        }))
+    }
 }
 
 const server=http.createServer(handler)
